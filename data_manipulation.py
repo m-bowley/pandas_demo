eaedd53e1355f2d0ae57e2a6596b178a424ee0a5
m-bowley <=>
2020-04-02 15:24:26 +0100

    Adding file to repository

--- data_manipulation.py
+++ data_manipulation.py
@@ -2,4 +2,8 @@ import pandas as pd
 
 df = pd.read_csv('data.csv')
 
-print(df)
+learner_id = df.iloc[0]["learner_id"]
+
+new_df = df[df["learner_id"] == learner_id]
+
+print(new_df)
--- get_fl_data.py
+++ get_fl_data.py
@@ -2,8 +2,8 @@ from fl_data_downloader import FutureLearnData
 
 fl = FutureLearnData("raspberry-pi")
 
-dataset_df = fl.get_dataset(course="web-development", run=1, dataset="step-activity")
+dataset_df = fl.get_dataset(course="introduction-to-web-developmentmac@raspberrypi.org", run=1, dataset="comments")
 
 print(dataset_df)
 
-dataset.to_csv('data.csv', index = False, header=True)
+dataset_df.to_csv('data.csv', index = False, header=True)
